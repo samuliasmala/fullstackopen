e13a2c28944ba0a4a64a5d73d2522b034727fa27
Samuli Asmala <samuli.asmala@gmail.com>
2023-03-27 19:45:11 +0300

    Add solution to 2.6-2.10

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,93 @@
+import { useState } from 'react';
+
+const App = () => {
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState('');
+
+  const addNewName = (event) => {
+    event.preventDefault();
+    if (newName === '') return;
+
+    if (persons.some((person) => person.name === newName))
+      return alert(`${newName} is already added to phonebook`);
+
+    setPersons((prevPersons) =>
+      prevPersons.concat({
+        id: prevPersons.length + 1,
+        name: newName,
+        number: newNumber,
+      })
+    );
+    setNewName('');
+    setNewNumber('');
+  };
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <Filter filter={filter} setFilter={setFilter} />
+      <h2>add a new</h2>
+      <PersonForm
+        addNewName={addNewName}
+        newName={newName}
+        setNewName={setNewName}
+        newNumber={newNumber}
+        setNewNumber={setNewNumber}
+      />
+      <h2>Numbers</h2>
+      <Persons persons={persons} filter={filter} />
+    </div>
+  );
+};
+
+export default App;
+
+const Filter = ({ filter, setFilter }) => (
+  <>
+    filter shown with:
+    <input value={filter} onChange={(event) => setFilter(event.target.value)} />
+  </>
+);
+
+const PersonForm = ({
+  addNewName,
+  newName,
+  setNewName,
+  newNumber,
+  setNewNumber,
+}) => (
+  <form onSubmit={addNewName}>
+    <div>
+      name:
+      <input
+        value={newName}
+        onChange={(event) => setNewName(event.target.value)}
+      />
+      <div>
+        number:
+        <input
+          value={newNumber}
+          onChange={(event) => setNewNumber(event.target.value)}
+        />
+      </div>
+    </div>
+    <div>
+      <button type="submit">add</button>
+    </div>
+  </form>
+);
+
+const Persons = ({ persons, filter }) =>
+  persons
+    .filter(
+      (person) =>
+        filter === '' ||
+        person.name.toLowerCase().indexOf(filter.toLowerCase()) >= 0
+    )
+    .map((person) => (
+      <div key={person.id}>
+        {person.name} {person.number}
+      </div>
+    ));

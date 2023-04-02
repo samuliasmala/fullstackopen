82d4a2a8f03274addc36730ba0227f7f14295ba0
Samuli Asmala <samuli.asmala@gmail.com>
2023-04-02 15:47:19 +0300

    Add solution to 3.2-3.3

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -28,6 +28,18 @@ app.get("/api/persons", (req, res) => {
   res.json(persons);
 });
 
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((p) => p.id === id);
+  if (!person) return res.status(404).end();
+  res.json(person);
+});
+
+app.get("/info", (req, res) => {
+  res.send(`<p>Phonebook has info for ${persons.length} people</p>
+            <p>${new Date().toString()}</p>`);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);

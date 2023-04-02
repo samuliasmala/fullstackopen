3c7af19001fd5618095cddadb398b14539c5e7aa
Samuli Asmala <samuli.asmala@gmail.com>
2023-04-02 16:22:43 +0300

    Add prettier and use single quotes

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,27 +1,27 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const morgan = require("morgan");
+const morgan = require('morgan');
 
 let persons = [
   {
     id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
+    name: 'Arto Hellas',
+    number: '040-123456',
   },
   {
     id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
   },
   {
     id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
+    name: 'Dan Abramov',
+    number: '12-43-234345',
   },
   {
     id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
   },
 ];
 
@@ -29,27 +29,27 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-morgan.token("body", (req, res) =>
-  req.method === "POST" ? JSON.stringify(req.body) : null
+morgan.token('body', (req, res) =>
+  req.method === 'POST' ? JSON.stringify(req.body) : null
 );
 
 const tinyWithBody =
-  ":method :url :status :res[content-length] - :response-time ms :body";
+  ':method :url :status :res[content-length] - :response-time ms :body';
 
 app.use(express.json());
 app.use(morgan(tinyWithBody));
 
-app.get("/api/persons", (req, res) => {
+app.get('/api/persons', (req, res) => {
   res.json(persons);
 });
 
-app.post("/api/persons", (req, res) => {
+app.post('/api/persons', (req, res) => {
   const { name, number } = req.body;
 
-  if (!name) return res.status(400).json({ error: "name is missing" });
-  if (!number) return res.status(400).json({ error: "number is missing" });
+  if (!name) return res.status(400).json({ error: 'name is missing' });
+  if (!number) return res.status(400).json({ error: 'number is missing' });
   if (persons.some((p) => p.name === name))
-    return res.status(400).json({ error: "name must be unique" });
+    return res.status(400).json({ error: 'name must be unique' });
 
   const person = {
     name,
@@ -61,20 +61,20 @@ app.post("/api/persons", (req, res) => {
   res.json(person);
 });
 
-app.get("/api/persons/:id", (req, res) => {
+app.get('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id);
   const person = persons.find((p) => p.id === id);
   if (!person) return res.status(404).end();
   res.json(person);
 });
 
-app.delete("/api/persons/:id", (req, res) => {
+app.delete('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id);
   persons = persons.filter((p) => p.id !== id);
   res.status(204).end();
 });
 
-app.get("/info", (req, res) => {
+app.get('/info', (req, res) => {
   res.send(`<p>Phonebook has info for ${persons.length} people</p>
             <p>${new Date().toString()}</p>`);
 });

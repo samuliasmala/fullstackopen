319c2360406bf3b81b07616bba72338554558b74
Samuli Asmala <samuli.asmala@gmail.com>
2023-07-10 13:16:05 +0300

    Use express-async-errors

--- part4/bloglist_backend/controllers/blogs.js
+++ part4/bloglist_backend/controllers/blogs.js
@@ -1,23 +1,15 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 
-blogsRouter.get('/', async (req, res, next) => {
-  try {
-    const blogs = await Blog.find({});
-    return res.json(blogs);
-  } catch (err) {
-    next(err);
-  }
+blogsRouter.get('/', async (req, res) => {
+  const blogs = await Blog.find({});
+  return res.json(blogs);
 });
 
-blogsRouter.post('/', async (req, res, next) => {
-  try {
-    const blog = new Blog(req.body);
-    const result = await blog.save();
-    return res.status(201).json(result);
-  } catch (err) {
-    next(err);
-  }
+blogsRouter.post('/', async (req, res) => {
+  const blog = new Blog(req.body);
+  const result = await blog.save();
+  return res.status(201).json(result);
 });
 
 module.exports = blogsRouter;

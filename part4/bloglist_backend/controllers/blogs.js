2e235c31287e2a55c0ca30d87703a6e7ba657101
Samuli Asmala <samuli.asmala@gmail.com>
2023-07-12 00:06:43 +0300

    Add solution to 4.17

--- part4/bloglist_backend/controllers/blogs.js
+++ part4/bloglist_backend/controllers/blogs.js
@@ -1,8 +1,13 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
+const User = require('../models/user');
 
 blogsRouter.get('/', async (req, res) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate('user', {
+    username: 1,
+    name: 1,
+    id: 1,
+  });
   return res.json(blogs);
 });
 
@@ -10,9 +15,16 @@ blogsRouter.post('/', async (req, res) => {
   if (req.body.title === undefined || req.body.url === undefined)
     return res.status(400).json({ error: 'title and url are required' });
 
-  const blog = new Blog({ likes: 0, ...req.body });
-  const result = await blog.save();
-  return res.status(201).json(result);
+  const users = await User.find({});
+  const user = users[0];
+
+  const blog = new Blog({ likes: 0, user: user.id, ...req.body });
+  const savedBlog = await blog.save();
+
+  user.blogs = user.blogs.concat(savedBlog._id);
+  await user.save();
+
+  return res.status(201).json(savedBlog);
 });
 
 blogsRouter.put('/:id', async (req, res) => {

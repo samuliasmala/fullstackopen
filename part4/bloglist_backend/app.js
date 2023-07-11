259d101aae292700b9e17fabad2376072c3b7904
Samuli Asmala <samuli.asmala@gmail.com>
2023-07-11 06:46:31 +0300

    Add solution to 4.15

--- part4/bloglist_backend/app.js
+++ part4/bloglist_backend/app.js
@@ -4,6 +4,7 @@ const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
 const blogsRouter = require('./controllers/blogs');
+const usersRouter = require('./controllers/users');
 const { MONGODB_URI } = require('./utils/config');
 const logger = require('./utils/logger');
 const middleware = require('./utils/middleware');
@@ -22,6 +23,7 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 
 app.use('/api/blogs', blogsRouter);
+app.use('/api/users', usersRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);

b04a8819e9a4e0fbff0ce1750b428fb2394c235a
Samuli Asmala <samuli.asmala@gmail.com>
2023-07-17 09:18:00 +0300

    Refactor LoginForm

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,12 +1,10 @@
 import { useState, useEffect } from 'react';
 import Blog from './components/Blog';
 import blogService from './services/blogs';
-import loginService from './services/login';
+import { LoginForm } from './components/LoginForm';
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
   const [user, setUser] = useState(null);
 
   useEffect(() => {
@@ -21,58 +19,12 @@ const App = () => {
     }
   }, []);
 
-  const handleLogin = async (event) => {
-    event.preventDefault();
-    try {
-      const user = await loginService.login({
-        username,
-        password,
-      });
-      window.localStorage.setItem('loggedBlogappUser', JSON.stringify(user));
-      setUser(user);
-      setUsername('');
-      setPassword('');
-    } catch (exception) {
-      console.log('Error: wrong credentials');
-      //setErrorMessage('wrong credentials')
-      setTimeout(() => {
-        //setErrorMessage(null)
-      }, 5000);
-    }
-  };
-
   const handleLogout = (event) => {
     window.localStorage.removeItem('loggedBlogappUser');
     setUser(null);
   };
 
-  if (user === null)
-    return (
-      <div>
-        <h2>Log in to application</h2>
-        <form onSubmit={handleLogin}>
-          <div>
-            username
-            <input
-              type="text"
-              value={username}
-              name="Username"
-              onChange={({ target }) => setUsername(target.value)}
-            />
-          </div>
-          <div>
-            password
-            <input
-              type="password"
-              value={password}
-              name="Password"
-              onChange={({ target }) => setPassword(target.value)}
-            />
-          </div>
-          <button type="submit">login</button>
-        </form>
-      </div>
-    );
+  if (user === null) return <LoginForm setUser={setUser} />;
 
   return (
     <div>
---
+++ part5/bloglist-frontend/src/components/LoginForm.js
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import loginService from '../services/login';
+
+export const LoginForm = ({ setUser }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      });
+      window.localStorage.setItem('loggedBlogappUser', JSON.stringify(user));
+      setUser(user);
+      setUsername('');
+      setPassword('');
+    } catch (exception) {
+      console.log('Error: wrong credentials');
+      //setErrorMessage('wrong credentials')
+      setTimeout(() => {
+        //setErrorMessage(null)
+      }, 5000);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Log in to application</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+  );
+};

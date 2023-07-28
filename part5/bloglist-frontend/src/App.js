ebb97ad8c2a85cc75aecca213c79e9cd0cc88f3e
Samuli Asmala <samuli.asmala@gmail.com>
2023-07-28 18:53:15 +0300

    Add solution to 5.10

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -67,9 +67,11 @@ const App = () => {
         />
       </Togglable>
 
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} increaseLikes={increaseLikes} />
-      ))}
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map((blog) => (
+          <Blog key={blog.id} blog={blog} increaseLikes={increaseLikes} />
+        ))}
     </div>
   );
 };

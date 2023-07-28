80f36b1b57a20ab5fc63a15099db3b3656b946fa
Samuli Asmala <samuli.asmala@gmail.com>
2023-07-28 18:16:49 +0300

    Add solution to 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,36 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+import { useState } from 'react';
+
+const Blog = ({ blog }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}{' '}
+        {isOpen ? (
+          <>
+            <button onClick={() => setIsOpen(false)}>hide</button>
+            <div>{blog.url}</div>
+            <div>
+              likes {blog.likes} <button>like</button>
+            </div>
+            <div>{blog.user?.name}</div>
+          </>
+        ) : (
+          <button onClick={() => setIsOpen(true)}>view</button>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default Blog;
